1a29e4f15a1d54aeaabbcca58082b2b72bfcc445
apanettiere <ah.panettiere@ufl.edu>
2023-12-08 16:47:50 -0500

    updated webiste

--- drewsmobiledetailing/src/components/container/Course/Services.jsx
+++ drewsmobiledetailing/src/components/container/Course/Services.jsx
@@ -1,7 +1,20 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import { motion } from "framer-motion";
 
 const Services = ({ image, title, content }) => {
+  const [isLargeScreen, setIsLargeScreen] = useState(window.innerWidth > 1060); // Assuming 1060px as the breakpoint for smaller devices
+
+  useEffect(() => {
+    const handleResize = () => {
+      setIsLargeScreen(window.innerWidth > 1060);
+    };
+    window.addEventListener("resize", handleResize);
+
+    return () => {
+      window.removeEventListener("resize", handleResize);
+    };
+  }, []);
+
   const item = {
     hidden: { y: 0, opacity: 0 },
     visible: { y: 0, opacity: 1 },
@@ -10,7 +23,7 @@ const Services = ({ image, title, content }) => {
   return (
     <motion.div
       variants={item}
-      whileHover={{ scale: 1.1 }}
+      whileHover={isLargeScreen ? { scale: 1.1 } : {}}
       className="flex items-center flex-col gap-4 bg-white p-8 rounded-md"
     >
       <div>
